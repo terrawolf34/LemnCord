d9d9170b8b579af6d9f2c46b785c34d25ec4490e
terrawolf34 <terrawolfiscool@gmail.com>
2023-02-10 20:26:01 +0200

    commitee

--- menu.py
+++ menu.py
@@ -1,19 +1,16 @@
 import os
-
-print("""
-╭╮╱╱╱╱╱╱╱╱╱╱╱╭━━━╮╱╱╱╱╱╱╭╮
+print("""╭╮╱╱╱╱╱╱╱╱╱╱╱╭━━━╮╱╱╱╱╱╱╭╮
 ┃┃╱╱╱╱╱╱╱╱╱╱╱┃╭━╮┃╱╱╱╱╱╱┃┃
 ┃┃╱╱╭━━┳╮╭┳━╮┃┃╱╰╋━━┳━┳━╯┃
 ┃┃╱╭┫┃━┫╰╯┃╭╮┫┃╱╭┫╭╮┃╭┫╭╮┃
 ┃╰━╯┃┃━┫┃┃┃┃┃┃╰━╯┃╰╯┃┃┃╰╯┃
-╰━━━┻━━┻┻┻┻╯╰┻━━━┻━━┻╯╰━━╯
-""")
+╰━━━┻━━┻┻┻┻╯╰┻━━━┻━━┻╯╰━━╯""")
 root_directory = os.getcwd()
 client_directory = os.path.join(root_directory, "Packets/Client")
 server_directory = os.path.join(root_directory, "Packets/Server")
 
 while True:
-    menu = input('Command list:\nSend message - send message \nGet message - get 100 messages\nJoin server - join server\nGo online - go online\nDm message - send dm\nLeave server - leave server\nEnter command: ')
+    menu = input('Command list:\nSend message/dm messagee - send dm/message \nGet message - get 100 messages\nJoin server/leave server - join server/leave server\nGo online - go online\nServer list/Channel list - get servers/channel list\nEnter command: ')
     if menu == 'send message':
         os.chdir(client_directory)
         os.system('python3 send_message.py')
@@ -45,7 +42,7 @@ while True:
         os.chdir(root_directory)
         
     if menu == 'leave server':
-    	os.chdir(server_directory)
+    	os.chdir(client_directory)
     	os.system('python3 leave_server.py')
     	os.chdir(root_directory)
     	
@@ -54,4 +51,20 @@ while True:
     	os.chdir(client_directory)
     	os.system('python3 3.py')
     	os.chdir(root_directory)
-  
+    	
+    
+    if menu == "server list":
+        os.chdir(server_directory)
+        os.system('python3 get_servers.py')
+        os.chdir(root_directory)
+        
+        
+    if menu == "channel list":
+        os.chdir(server_directory)
+        os.system('python3 get_channels.py')
+        os.chdir(root_directory)
+        
+    if menu == "upload file":
+        os.chdir(client_directory)
+        os.system('python3 file_upload.py')
+        os.chdir(root_directory)
