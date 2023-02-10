a125db6c6ab2d37e5612d0fb40d9bfc2defca2e8
terrawolf34 <terrawolfiscool@gmail.com>
2023-02-10 21:00:29 +0200

    fixed hopefullllllllllllyyyyyyyyyyyyy

--- Packets/Server/leave_server.py
+++ Packets/Server/leave_server.py
@@ -4,21 +4,19 @@ sys.path.append("../..")
 from bruh import token
 
 
-
-endpoint = "https://discord.com/api/v6/users/@me/guilds/{guild_id}"
-
-
-endpoint = endpoint.format(guild_id=input("server id?"))
-
 headers = {
-    "Authorization": "Bearer {}".format(token),
+    "Authorization": token
 }
 
-# Send a DELETE request to the API endpoint
-response = requests.delete(endpoint, headers=headers)
+def leave_server(guild_id):
+    response = requests.delete(f"https://discord.com/api/v6/users/@me/guilds/{guild_id}", headers=headers)
+    if response.status_code == 204:
+        return True
+    else:
+        return False
 
-# Check the response code to see if the request was successful
-if response.status_code == 204:
-    print("Successfully left the server!")
+guild_id = input("Enter the ID of the server you want to leave: ")
+if leave_server(guild_id):
+    print("Left server successfully!")
 else:
-    print("Failed to leave the server. Response: {}".format(response.text))
+    print("Could not leave server. Check your token and try again.")
