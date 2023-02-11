a1bc4da97905083831345e4f87b75bb3510c02e5
terrawolf34 <terrawolfiscool@gmail.com>
2023-02-11 02:11:15 -0500

    tongue cat way

--- menu.py
+++ menu.py
@@ -10,7 +10,7 @@ client_directory = os.path.join(root_directory, "Packets/Client")
 server_directory = os.path.join(root_directory, "Packets/Server")
 
 while True:
-    menu = input('Command list:\nSend message/dm messagee - send dm/message \nGet message - get 100 messages\nJoin server/leave server - join server/leave server\nGo online - go online\nServer list/Channel list - get servers/channel list\nChange about me - change about me\nEnter command: ')
+    menu = input('Command list:\nSend message/dm message - send dm/message \nGet message/get dm message - get (dm) 100 messages\nJoin server/leave server - join server/leave server\nGo online - go online\nServer list/Channel list/Dm list - get servers/channel list/dm list\nChange about me - change about me\nEnter command: ')
     if menu == 'send message':
         os.chdir(client_directory)
         os.system('python3 send_message.py')
@@ -72,4 +72,16 @@ while True:
     if menu == "change about me":
         os.chdir(client_directory)
         os.system('python3 change_status.py')
+        os.chdir(root_directory)
+        
+        
+    if menu == "dm list":
+        os.chdir(server_directory)
+        os.system('python3 get_channels.py')
+        os.chdir(root_directory)
+        
+        
+    if menu == "get dm message":
+        os.chdir(server_directory)
+        os.system('python3 get_channels.py')
         os.chdir(root_directory)
