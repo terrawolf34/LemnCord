0336f7405461a0757347a786d3838320e2b567ea
Solar <115794865+Solaree@users.noreply.github.com>
2023-02-12 21:57:03 +0200

    Update edit_message.py

--- Packets/Client/edit_message.py
+++ Packets/Client/edit_message.py
@@ -1,30 +1,31 @@
-import requests
-import sys
+import requests, sys; from Utils.Reader import *
 sys.path.append("../..")
-from bruh import token
 
-headers = {
-    "Authorization": token
-}
+def edit_message():
+    Reader()
 
-# Replace the following with the ID of the channel you want to edit a message in
-channel_id = input("Channel id? ")
+    headers = {
+        "Authorization": token
+    }
 
-# Replace the following with the ID of the message you want to edit
-message_id = input("MessageID? ")
+    # Replace the following with the ID of the channel you want to edit a message in
+    channel_id = input("Channel id? ")
 
-# The new content of the message
-new_content = input("edited message content?")
+    # Replace the following with the ID of the message you want to edit
+    message_id = input("MessageID? ")
 
-# Send a PATCH request to the Discord API to edit the message
-response = requests.patch(
-    f"https://discord.com/api/v6/channels/{channel_id}/messages/{message_id}",
-    headers=headers,
-    json={"content": new_content},
-)
+    # The new content of the message
+    new_content = input("edited message content?")
 
-# Check if the request was successful
-if response.status_code == 200:
-    print("Message successfully edited")
-else:
-    print("Failed to edit message")
+    # Send a PATCH request to the Discord API to edit the message
+    response = requests.patch(
+        f"https://discord.com/api/v6/channels/{channel_id}/messages/{message_id}",
+        headers=headers,
+        json={"content": new_content},
+    )
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        print("Message successfully edited")
+    else:
+        print("Failed to edit message")
