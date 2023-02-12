e92341a0afcf33d3c9b3dcccd705eef94526e712
Solar <115794865+Solaree@users.noreply.github.com>
2023-02-12 10:59:50 -0800

    Update change_status.py

--- Packets/Client/change_status.py
+++ Packets/Client/change_status.py
@@ -1,25 +1,22 @@
-import requests
-import json
-import sys
+import requests, json, sys; from Packets.Client.send_message import *
 sys.path.append("../..")
-from bruh import token
+def change_status():
+    Reader() #token
+    headers = {
+        "Authorization": token,
+        "Content-Type": "application/json"
+    }
 
+    def change_about(about):
+        data = {"description": about}
+        response = requests.patch("https://discord.com/api/v6/users/@me", headers=headers, data=json.dumps(data))
+        if response.status_code == 200:
+            return True
+        else:
+            return False
 
-headers = {
-    "Authorization": token,
-    "Content-Type": "application/json"
-}
-
-def change_about(about):
-    data = {"description": about}
-    response = requests.patch("https://discord.com/api/v6/users/@me", headers=headers, data=json.dumps(data))
-    if response.status_code == 200:
-        return True
+    about = input("Enter your desired about text: ")
+    if change_about(about):
+        print("About text changed successfully!")
     else:
-        return False
-
-about = input("Enter your desired about text: ")
-if change_about(about):
-    print("About text changed successfully!")
-else:
-    print("Could not change about text. Check your token and try again.")
+        print("Could not change about text. Check your token and try again.")
