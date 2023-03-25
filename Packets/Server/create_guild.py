21b4a38368e8765bfec027253be67320510a12b9
terrawolf34 <terrawolfiscool@gmail.com>
2023-03-25 16:40:59 +0200

    wtf???????? new update????????????????????????????????????????????????????
    sdadasdwdasd

---
+++ Packets/Server/create_guild.py
@@ -0,0 +1,22 @@
+import requests
+import sys
+sys.path.append("../..")
+from bruh import token
+tokenizor = token
+guild_name = input("guild name? ")
+
+# Define the headers to include the user token
+headers = {
+    "Authorization": f"{tokenizor}"
+}
+
+# Define the JSON payload to create the server
+payload = {
+    "name": f"{guild_name}"
+}
+
+# Send the POST request to create the server
+response = requests.post("https://discord.com/api/v9/guilds", headers=headers, json=payload)
+
+# Print the response
+print(response.json())
